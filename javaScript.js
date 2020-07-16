134bc97d8ee562597439b5f05f0f8f402a304bbc
Tiago Zozimo <tiagonoiid@gmail.com>
2020-07-16 10:32:20 -0300

    Perguntas ok.

---
+++ javaScript.js
@@ -0,0 +1,61 @@
+/*
+6° Dentro do arquivo javascript, preciso que me explique da forma que você achar melhor o que é:
+
+    *6.1 variáveis e constantes.
+            Váriaveis são dados armazenados em memoria onde sua diferença consiste em que
+            variaveis podem ter seu valor alterado.
+            Uma constante também é uma variavel a diferença é que seu valor é único,
+            não mudará com o tempo de execução do programa.
+
+    *6.2 comandos de decisão.
+            São instruções onde sua tomada de decisão depende de se uma condição
+            como, verdadeiro ou falso for atendida.
+
+    *6.3 loops.
+            São instruções que se repetirá varias vezes até que uma condição seja atendida.
+
+    *6.4 funções
+            É uma parte do código qual criamos para que seja chamada quando necessária.
+
+    *6.5 escopos
+            Escopos podemos definir a qual local em nosso codigo poderá ser acessada,
+            em js basicamente temos dois tipos, global e local.
+
+    *6.6 inputs e outputs
+        input é entrada de informação, output é saída ou envio de informação.
+
+    *6.7 array
+            cadeia de elementos, matriz, vetor
+
+    *6.8 objetos
+            objetos sao coisas com propriedades que definem o que aquilo é.
+
+    *6.9 (camelCase, PascalCase, kibab-case, snake_case).
+            Case style das linguagens.
+
+    *6.10 Diferença entre caminho absoluto e caminho relativo.
+            Absoluto é necessario indicar mais especificamente o diretorio, a raiz
+            relativo não.
+
+    *6.11 Diferença entre os operadores &&, ||, == e !=
+            && e
+            || ou
+            == igual
+            != diferente
+
+    *6.12 O que é EPER, e para que serve.
+            Entender, Planejar, Executar e Revisar.
+            Estratégia de organização.
+
+
+    *6.13 Explique o que é Pilha e Fila.
+            Estrutuda de dados onde a fila tem os dados primeiro que entra e primeiro que sai. (FIFO)
+            Pilha primeiro que entra e último que sai.(LIFO)
+
+    *6.14 Explique com suas palavras qual a diferença entre html, javascript e css
+            HTML é linguagem de marcaçãod e texto, esqueleto.
+            CSS é camada de estilo em folhas onde demos a forma.
+            E JavaScript é uma ling de programação onde damos a funcionalidade, comportamento.
+
+
+/
